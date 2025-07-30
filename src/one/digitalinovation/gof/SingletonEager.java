641ba6e7787fcd89f82e45f21df5f0f990fabc9b
Your Name <wendelsil@gmail.com>
2025-07-30 16:44:28 -0400

    Adiciona implementação dos padrões Singleton: Eager, Lazy e Lazy Holder, além de um teste para validação.

---
+++ src/one/digitalinovation/gof/SingletonEager.java
@@ -0,0 +1,16 @@
+package one.digitalinovation.gof;
+
+/**
+ * Singleton "apressado"
+ */
+public class SingletonEager {
+    private static SingletonEager instancia = new SingletonEager();
+
+    private SingletonEager(){
+        super();
+    }
+    public static SingletonEager getInstancia(){
+        return instancia;
+    }
+
+}
---
+++ src/one/digitalinovation/gof/Test.java
@@ -0,0 +1,21 @@
+package one.digitalinovation.gof;
+
+public class Test {
+    public static void main(String[] args) {
+        SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+        eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+
+        SingletonLazyHolder lazyholder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyholder);
+        lazyholder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyholder);
+    }
+
+}
